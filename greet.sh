11bb0ceefb0a3f09b6af2eb47b4739a3ce558ce9
Vlad <vladseremet4@gmail.com>
2026-02-05 16:09:45 +0000

    updated mkdir with quotes

--- greet.sh
+++ greet.sh
@@ -9,5 +9,5 @@ echo "What should we call your new project, $username ?"
 read project_name
 
 # Create new project folder
-mkdir $project_name
+mkdir "$project_name"
 echo "READY! Folder '$project_name' has been created for $username"
