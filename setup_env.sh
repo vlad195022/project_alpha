d1e277d4715667abd5d29533d5fe5c0a660dfdb2
Vlad <vladseremet4@gmail.com>
2026-02-05 11:20:00 +0000

    Arrange user's environment

---
+++ setup_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Privet, $USER! Starting setup the environment"
+mkdir -p docs logs scripts
+touch docs/readme.txt logs/install.log
+echo "Folders are successfully created on $(date)"
