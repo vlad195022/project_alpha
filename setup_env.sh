1d2a6447b7b030f116c1600217170ef7d86cdb7c
Vlad <vladseremet4@gmail.com>
2026-02-05 12:28:31 +0000

    Added existence check for logs folder

--- setup_env.sh
+++ setup_env.sh
@@ -4,3 +4,16 @@ echo "Privet, $USER! Starting setup the environment"
 mkdir -p docs logs scripts
 touch docs/readme.txt logs/install.log
 echo "Folders are successfully created on $(date)"
+
+# add logic, creating variable with folder name
+FOLDER="logs"
+
+if [ -d "$FOLDER" ] 
+then
+	echo "Checking: Folder $FOLDER  successfully has been found"
+	ls -l $FOLDER
+else
+	echo "Error: Something went wrong, folder $FOLDER wasn't created"
+fi
+
+echo "Processing complete on $(date)"
